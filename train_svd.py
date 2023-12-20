3ae666698a4d71c3794c951537e1f37e21e7a949
BlakeOne <30273164+BlakeOne@users.noreply.github.com>
2023-12-19 21:02:27 -0800

    Update train_svd.py

--- train_svd.py
+++ train_svd.py
@@ -1013,7 +1013,7 @@ def main():
                 noisy_latents = latents + noise * sigmas
                 timesteps = torch.Tensor(
                     [0.25 * sigma.log() for sigma in sigmas]).to(accelerator.device)
- 
+
                 inp_noisy_latents = noisy_latents / ((sigmas**2 + 1) ** 0.5)
 
                 # Get the text embedding for conditioning.
